f5f21b57d756958fc5be5efcba4deaba20fccee3
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2020-03-16 17:57:13 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React with Intercom!
+      </header>
+
+      <p>
+        For more, visit our <a href="https://developers.intercom.com/installing-intercom/docs/intercom-javascript">documentation</a> or reach us using the Messenger at <a href="www.intercom.com">www.intercom.com</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
