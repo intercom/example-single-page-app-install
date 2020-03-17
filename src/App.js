1fd4526ea4feadb2ed5e167ef52bb6f1749585a7
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2020-03-17 11:01:59 -0700

    Add example for how to use with hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,63 @@
-import React from 'react';
+import React from "react"
 import { Router, Link } from "@reach/router"
-import './App.css';
+import {
+  load as loadIntercom,
+  boot as bootIntercom,
+  useIntercom
+} from "./intercom"
+import "./App.css"
 
 const Home = () => {
-  return <>
-    <header className="App-header">
-      React with Intercom!
-    </header>
+  useIntercom()
+  return (
+    <>
+      <header className="App-header">React with Intercom!</header>
 
-    <p>
-      For more, visit our <a href="https://developers.intercom.com/installing-intercom/docs/intercom-javascript">documentation</a> or reach us using the Messenger at <a href="www.intercom.com">www.intercom.com</a>
-    </p>
-  </>
+      <p>
+        For more, visit our{" "}
+        <a href="https://developers.intercom.com/installing-intercom/docs/basic-javascript#section-single-page-app">
+          documentation
+        </a>{" "}
+        or reach us using the Messenger at{" "}
+        <a href="www.intercom.com">www.intercom.com</a>
+      </p>
+    </>
+  )
 }
 
 const About = () => {
-  return <>
-    <header className="App-header">
-      Example about page
-    </header>
+  useIntercom()
+  return (
+    <>
+      <header className="App-header">Example about page</header>
 
-    <p>
-      See <code>App.js</code> to see how to hook into routing to send updates to
-      Intercom on page changes.
-    </p>
-  </>
+      <p>
+        See <code>App.js</code> to see how to hook into routing to send updates
+        to Intercom on page changes.
+      </p>
+    </>
+  )
 }
 
 function App() {
+  loadIntercom()
+  bootIntercom()
+
   return (
     <div className="App">
       <nav className="App-nav">
-        Try changing pages <span role="img" aria-label="point right">👉</span>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="about">About</Link>
+        Try changing pages{" "}
+        <span role="img" aria-label="point right">
+          👉
+        </span>
+        <Link to="/">Home</Link> | <Link to="about">About</Link>
       </nav>
       <Router>
         <Home path="/" />
         <About path="about" />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
